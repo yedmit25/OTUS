0bc53a2c8fbc31a46e8ec1cd84adaba2c789d63b
eda82 <yedmit@yandex.ru>
2024-08-04 03:05:45 +0300

    OTUS-1 Интерактивное меню.

---
+++ InteractiveMenuTelegramBot/InteractiveMenuTelegramBot/MenuItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractiveMenuTelegramBot
+{
+    internal class MenuItems
+    {
+        public const string Start = "/start";
+        public const string Exit = "/exit";
+        public const string Help = "/help";
+        public const string Echo = "/echo";
+    }
+}
---
+++ InteractiveMenuTelegramBot/InteractiveMenuTelegramBot/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace InteractiveMenuTelegramBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? userName;
+            string? commandValue;
+            Console.WriteLine("Добро пожаловать! Это Telegram - бот. Для вас доступны команды /start, /help, /info, /exit.");
+            do
+            {
+                commandValue = Console.ReadLine();
+                parseCommand(commandValue);
+            }
+            while (commandValue != MenuItems.Exit);
+
+            void parseCommand(string? command)
+            {
+                switch (command)
+                {
+                    case MenuItems.Start:
+                        Console.WriteLine("Введите имя пользователя");
+                        userName = Console.ReadLine();
+                        break;
+                    case MenuItems.Help:
+                        Console.WriteLine("Помогите");
+                        break;
+                    default:
+                        Console.WriteLine("Пока");
+                        break;
+                }
+
+            }
+        }
+    }
+}
