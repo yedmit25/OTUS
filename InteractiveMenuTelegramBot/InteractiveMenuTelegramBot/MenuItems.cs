1b9bea303769e824911ecafd7b5818dc0ca946fa
eda82 <yedmit@yandex.ru>
2024-08-11 14:11:03 +0300

    OTUS-1 доработка задачи

--- InteractiveMenuTelegramBot/InteractiveMenuTelegramBot/MenuItems.cs
+++ InteractiveMenuTelegramBot/InteractiveMenuTelegramBot/MenuItems.cs
@@ -11,6 +11,8 @@ namespace InteractiveMenuTelegramBot
         public const string Start = "/start";
         public const string Exit = "/exit";
         public const string Help = "/help";
+        public const string Info = "/info";
         public const string Echo = "/echo";
+        public const string HelpConstant = "Помощь:\n /sart - запуск работы бота;\n /help - справочная информация;\n /info - описание программы, версия, дата создания;\n /echo - вывод введённого сообщения пользователем;\n /exit - завершение работы бота";
     }
 }
--- InteractiveMenuTelegramBot/InteractiveMenuTelegramBot/Program.cs
+++ InteractiveMenuTelegramBot/InteractiveMenuTelegramBot/Program.cs
@@ -1,35 +1,55 @@
-﻿namespace InteractiveMenuTelegramBot
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.Design;
+using System.Reflection;
+
+namespace InteractiveMenuTelegramBot
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string? userName;
+            string? userName = "";
             string? commandValue;
-            Console.WriteLine("Добро пожаловать! Это Telegram - бот. Для вас доступны команды /start, /help, /info, /exit.");
+            Console.WriteLine($"Добро пожаловать! Это Telegram - бот. Для вас доступны команды {MenuItems.Start}, {MenuItems.Help}, {MenuItems.Info}, {MenuItems.Exit}.");
             do
             {
+                Console.Write("-->");
                 commandValue = Console.ReadLine();
-                parseCommand(commandValue);
+                executeCommand(commandValue);
             }
             while (commandValue != MenuItems.Exit);
 
-            void parseCommand(string? command)
+            void executeCommand(string? command)
             {
-                switch (command)
+                if (command != null)
                 {
-                    case MenuItems.Start:
-                        Console.WriteLine("Введите имя пользователя");
-                        userName = Console.ReadLine();
-                        break;
-                    case MenuItems.Help:
-                        Console.WriteLine("Помогите");
-                        break;
-                    default:
-                        Console.WriteLine("Пока");
-                        break;
-                }
+                    if (userName != "" && command.TrimStart().StartsWith($"{MenuItems.Echo}"))
+                    {
+                        List<string> wordsOfCommand = new(command.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+                        wordsOfCommand.RemoveAt(0);
+                        string result = string.Join(" ", wordsOfCommand);
+                        Console.WriteLine(result);
+                    }
+                    switch (command)
+                    {
+                        case MenuItems.Start:
+                            Console.WriteLine("Введите имя пользователя");
+                            userName = Console.ReadLine();
+                            break;
+                        case MenuItems.Help:
+                                Console.WriteLine($"{(userName != "" ? userName + ",\n" : "")}{MenuItems.HelpConstant}");
+                            break;
+                        case MenuItems.Info:
+                            Console.WriteLine("info");
+                            Console.WriteLine(string.Format($"{(userName != "" ? userName + ",\n" : "")}Текущая версия системы: {Assembly.GetExecutingAssembly().GetName().Version}\nДата создания: {Utils.GetBuildDate(Assembly.GetExecutingAssembly()):d}"));
+                            break;
+                        case MenuItems.Exit:
+                            Console.WriteLine($"Пока{(userName != "" ? ", " + userName : "")}");
+                            break;
+                    }
 
+
+                }
             }
         }
     }
