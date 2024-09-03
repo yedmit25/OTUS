387d5d3890c6a5d724ad3d3a2797479f768116df
eda82 <yedmit@yandex.ru>
2024-09-03 03:19:47 +0300

    OTUS-4 Основное задание

--- ObjectClasses/ObjectClasses/Program.cs
+++ ObjectClasses/ObjectClasses/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var s = new Stack("asdf54", "b", "c", "d", "sdfsdf");
+
+            try
+            {
+
+                Console.WriteLine(s.Top);
+
+                s.Pop(out string ss);
+
+                Console.WriteLine(ss);
+
+                Console.WriteLine(s.Top);
+
+                var sm = new Stack();
+
+                Console.WriteLine(sm.Top);
+
+
+
+                sm.Pop(out string stm);
+                Console.WriteLine(stm);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
---
+++ ObjectClasses/ObjectClasses/Stack .cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ObjectClasses
+{
+    public class Stack : IEnumerable
+    {
+        public int Size 
+        { get
+            {
+                return _list.Count;
+            }
+        }
+
+        public string Top
+        {
+            get
+            {
+                return _list.FirstOrDefault();
+            }
+        }
+
+        public void Add(string val)
+        { _list.Add(val); }
+
+        public void Pop(out string item)
+        {
+
+            if (_list.Count > 0)
+            {
+
+                item = _list.First();
+
+                _list.RemoveAt(0);
+            } else
+            {
+                throw new Exception("Стек пустой");
+            }
+        } 
+    
+
+
+        private List<string> _list = new();
+
+        public Stack (params string[] val)
+        {
+            foreach (string s in val)
+            {
+                _list.Add(s);
+            }
+        }
+
+
+
+    public IEnumerator GetEnumerator()
+    {
+        return _list.GetEnumerator();
+    }
+}
+}
+
