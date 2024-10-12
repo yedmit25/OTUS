471b00f447431777e45a76028bbe678795cf94dc
eda82 <yedmit@yandex.ru>
2024-10-12 23:32:05 +0300

    OTUS-8 Бинарное дерево. Пример.

---
+++ HW_9_BinaryTree/HW_9_BinaryTree/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_9_BinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
