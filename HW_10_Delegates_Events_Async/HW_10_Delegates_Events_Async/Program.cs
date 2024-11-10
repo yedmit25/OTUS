a9c001d79763743c3acc944343613d5335465c7c
eda82 <yedmit@yandex.ru>
2024-11-10 09:47:17 +0300

    OTUS-10 Исправил битые ссылки на рабочие

--- HW_10_Delegates_Events_Async/HW_10_Delegates_Events_Async/Program.cs
+++ HW_10_Delegates_Events_Async/HW_10_Delegates_Events_Async/Program.cs
@@ -10,18 +10,19 @@
         static async Task Main(string[] args)
         {
             string[] imageUrls = {
-            "https://unsplash.com/photos/qkiCFFQp-Bg/download?force=true&w=2400",
-            // Добавьте другие 9 URL-адресов изображений здесь
-            "https://unsplash.com/photos/3JdubjN5xTI/download?force=true",
-            "https://unsplash.com/photos/H-1j_s0dhCw/download?force=true",
-            "https://unsplash.com/photos/nEvzSXBIhiU/download?force=true&w=2400",
-            "https://unsplash.com/photos/PSF2RhUBORs/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTR8fG5ldyUyMHplYWxhbmR8ZW58MHx8fHwxNzMxMTk2Mzg5fDA&force=true",
-            "https://unsplash.com/photos/75_s8iWHKLs/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MTl8fG5ldyUyMHplYWxhbmR8ZW58MHx8fHwxNzMxMTk2Mzg5fDA&force=true",
-            "https://unsplash.com/photos/eaRZniHTCD4/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Nzh8fG5ldyUyMHplYWxhbmR8ZW58MHx8fHwxNzMxMTg5MjM4fDA&force=true",
-            "https://unsplash.com/photos/Ton3nTyeAYI/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MjkyfHxuZXclMjB6ZWFsYW5kfGVufDB8fHx8MTczMTIxNjUyMnww&force=true",
-            "https://unsplash.com/photos/B5aGZ6kYCX4/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MzM5fHxuZXclMjB6ZWFsYW5kfGVufDB8fHx8MTczMTIxNjU3Nnww&force=true",
-            "https://unsplash.com/photos/Gb2LYrknHGE/download?force=true",
-            "https://unsplash.com/photos/8ZpLCdnxSBk/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NDA2fHxuZXclMjB6ZWFsYW5kfGVufDB8fHx8MTczMTIxNjY5Nnww&force=true"
+            "https://img3.akspic.ru/attachments/originals/4/1/1/3/0/103114-lug-priroda-holm-teatralnye_dekoracii-nebo-2844x1600.jpg",
+            "https://images.unsplash.com/photo-1560185007-c5ca9d2c014d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1560185007-5f0bb1866cab?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1676823553207-758c7a66e9bb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1556593825-c11de986cb0b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1560448205-17d3a46c84de?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1600494448850-6013c64ba722?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1560449752-8b6023e2ab5a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1643949914872-317d6047f107?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1560185009-dddeb820c7b7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "https://images.unsplash.com/photo-1533779283484-8ad4940aa3a8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         };
 
             Console.WriteLine("Начинаем загрузку 10 изображений...");
