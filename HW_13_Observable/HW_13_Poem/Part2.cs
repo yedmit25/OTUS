65470c96887e90d637fd6c94c7abf147cea1d9ef
eda82 <yedmit@yandex.ru>
2024-12-29 20:49:16 +0300

    OTUS-13 - задача поема "Дом, который построил Джек"

---
+++ HW_13_Observable/HW_13_Poem/Part2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_13_Poem
+{
+    internal class Part2
+    {
+        public List<string> Poem { get; private set; }
+
+        public List<string> AddPart(List<string> collection)
+        {
+            Poem = new List<string>(collection)
+        {
+            "А это пшеница,\nКоторая в темном чулане хранится\nВ доме,\nКоторый построил Джек."
+        };
+            return Poem;
+        }
+    }
+}
---
+++ HW_13_Observable/HW_13_Poem/Part6.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_13_Poem
+{
+    internal class Part6
+    {
+        public List<string> Poem { get; private set; }
+
+        public List<string> AddPart(List<string> collection)
+        {
+            Poem = new List<string>(collection)
+        {
+            "А это корова безрогая,\nЛягнувшая старого пса без хвоста,\nКоторый за шиворот треплет кота,\nКоторый пугает и ловит синицу,\nКоторая часто ворует пшеницу,\nКоторая в темном чулане хранится\nВ доме,\nКоторый построил Джек."
+        };
+            return Poem;
+        }
+    }
+}
